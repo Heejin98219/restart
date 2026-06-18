bb7c3fc22c42f4b029cccb203166711dc00893ab
whgmlwls0219 <whgmlwls0219@naver.com>
2026-06-18 23:52:41 +0900

    태그 (짐코딩 Ep04-3 05:38)

--- Ep03.js
+++ Ep03.js
@@ -1,3 +1,58 @@
-// Emmet
+s; // Emmet
 // html과 css의 자동 완성 기능 제공
 // 작성 시간 단축
+
+// 자식노드
+// '>' 사용
+// ex) div>ul>li 입력 후 탭 클릭 시
+// <div>
+//     <ul>
+//         <li></li>
+//     </ul>
+// </div>
+// 자동 출력
+
+// 형제노드
+// '+' 사용
+// ex) div>ol+ul+div 입력 후 탭 클릭 시
+// <div>
+//     <ol></ol>
+//     <ul></ul>
+//     <div></div>
+// </div>
+// 자동 출력
+
+// 반복노드
+// '*' 사용
+// ex) div>ul>li*3 입력 후 탭 클릭 시
+// <div>
+//    <ul>
+//        <li></li>
+//        <li></li>
+//        <li></li>
+//    </ul>
+// </div>
+
+// 아이디
+// '#' 사용
+// ex1) span#item 입력 시 <span id="item"></span>
+// ex2) #item만 입력 시 <div id="item"></div>
+// 태그의 기본 값은 div
+
+// 클래스
+// '.' 사용
+// ex1) span.title 입력 시 <span class="title></span>
+// ex2) .title만 입력 시 <div class="title"></div>
+
+// 컨텐츠
+// '{ }' 사용
+// ex) p.container{Hello World~!} 입력 시
+// <p class="container">Hello World~!</p> 출력
+
+// 자동 넘버링
+// '$' 사용
+// ex) p.container{item}
+// 단, item에는 넘버링이 부여되도록 할 것임
+// 그 때 '$' 사용
+// <p class="container">item1</p> 출력
+// ex2) 5개 반복 시 p.container{item$}*5 입력 후 탭 클릭
---
+++ Ep04_02.js
@@ -0,0 +1,26 @@
+// <ol> (Ordered List)
+// 순서가 있는 목록 표현
+
+// <ul> (Unordered List)
+// 순서가 없는 목록 표현
+// type 속성으로 글머리 기호 변경 가능
+
+// <li> (Listed Item)
+// 목록하위 항목
+// <ol> 또는 <ul> 태그의 하위에 위치
+
+// <dl> (Definition Item: 정의 목록)
+// 용어를 설명하는 목록을 만듦
+// ex) A는 B이다. 와 같은 Key = Value로 사용할 때 유용
+
+// <dt> (Definition Term: 정의 용어)
+// 정의되는 용어의 제목을 넣을 때 사용
+
+// <dd> (Definition Description: 정의 설명)
+// 용어를 설명하는 데 사용
+
+// <dl>태그는 하나 이상의 <dt>-<dd>쌍의 태그를 가져야 함
+// <dt>-<dd>태그가 반드시 하나의 짝으로 지어져야 하는 것은 아님
+// <li>, <dt>-<dd> 태그는 밖에서 독립적으로 사용 불가
+
+// <ul>태그 하위 요소로 <li>태그가 위치해야 함
