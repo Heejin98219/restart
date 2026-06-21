6823b62e8b15fe466ee715cca4f4b5af87652d41
whgmlwls0219 <whgmlwls0219@naver.com>
2026-06-21 23:52:54 +0900

    태그 (짐코딩 Ep07-1 04:20)

---
+++ Ep06.js
@@ -0,0 +1,42 @@
+// <div> (division: 분할)
+// 영역태그
+// 영역 분할 시 사용
+// 영역을 구분
+// 컨테이너
+// 다른 태그들을 포함
+
+// <span>
+// 영역태그
+// 특정 텍스트나 문장 영역 지정
+// 특정 아이템
+
+// display: block
+// 화면 구성이나 레이아웃을 짤 때 사용
+// 한 칸을 모두 차지
+// 세로로 나열
+// width, height, margin 속성 적용
+// <h1> ~ <h6>, <ol>, <ul>, <li>...
+
+// display: inline
+// 컨텐츠의 흐름을 끊지 않음
+// 태그에 할당된 공간만 차지
+// 가로로 나열
+// margin-top, margin-bottom, width, height, text-align 속성 미적용
+// <a>, <em>, <img>, <span> ...
+
+// 속성 값
+// none
+// 보이지 않음
+
+// block
+// block 요소로 표시
+
+// inline
+// inline 요소로 표시
+
+// inline-block
+// block과 ilnine의 중간 형태
+// 요소는 inline인데 내부는 block처럼 표시
+
+// flex
+// 레이아웃을 만들 때 자주 사용
